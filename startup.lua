e7c77e02d547ab45425d063480594478eee22e86
SchindlerShadow <SchindlerShadow@gmail.com>
2025-08-15 23:20:01 -0400

    fixed timer

--- startup.lua
+++ startup.lua
@@ -25,18 +25,20 @@ local lastSent = {}
 
 while true do
     local players = detector.getPlayersInRange(radius) or {}
-    local now = os.time()
 
     if next(players) ~= nil then
         for _, player in ipairs(players) do
-            if not lastSent[player] or (now - lastSent[player]) > cooldown then
+            --print("time " .. tostring( lastSent[player]))
+            if not lastSent[player] or (lastSent[player]) > cooldown then
                 print("Sending chat to ".. player)
                 chat.sendMessageToPlayer(
                     "Use \"/warp overworld\" to go to overworld", player)
                 --chat.sendMessageToPlayer("Discord: https://discord.gg/5vySCrG", player)
 
-                lastSent[player] = now
+                lastSent[player] = 0
                 sleep(1)
+                elseif lastSent[player] then
+                    lastSent[player] = lastSent[player] + 5
             end
         end
     end
