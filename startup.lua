eef095dd3e333347a6e293fc9a7eee7c2deb47ab
SchindlerShadow <SchindlerShadow@gmail.com>
2025-08-16 13:16:26 -0400

    added general chat replies

--- startup.lua
+++ startup.lua
@@ -23,25 +23,65 @@ end
 
 local lastSent = {}
 
-while true do
-    local players = detector.getPlayersInRange(radius) or {}
+local function spawnMessageLoop()
 
-    if next(players) ~= nil then
-        for _, player in ipairs(players) do
-            --print("time " .. tostring( lastSent[player]))
-            if not lastSent[player] or (lastSent[player]) > cooldown then
-                print("Sending chat to ".. player)
-                chat.sendMessageToPlayer(
-                    "Use \"/warp overworld\" to go to overworld", player)
-                --chat.sendMessageToPlayer("Discord: https://discord.gg/5vySCrG", player)
+    while true do
+        local players = detector.getPlayersInRange(radius) or {}
 
-                lastSent[player] = 0
-                sleep(1)
+        if next(players) ~= nil then
+            for _, player in ipairs(players) do
+                -- print("time " .. tostring( lastSent[player]))
+                if not lastSent[player] or (lastSent[player]) > cooldown then
+                    print("Sending chat to " .. player)
+                    chat.sendMessageToPlayer("Use \"/warp overworld\" to go to overworld", player, "Spawn Chatbox",
+                        "<>", "&b")
+                    -- chat.sendMessageToPlayer("Discord: https://discord.gg/5vySCrG", player)
+
+                    lastSent[player] = 0
+                    sleep(1)
                 elseif lastSent[player] then
                     lastSent[player] = lastSent[player] + 5
+                end
             end
         end
+
+        sleep(5)
+    end
+end
+
+local function monitorChat()
+    while true do
+        local event, username, message, uuid, isHidden = os.pullEvent("chat")
+
+        if message and message:lower():find("wiki") then
+            local message = {{
+                text = "InfiniX Wiki Link",
+                underlined = true,
+                color = "aqua",
+                clickEvent = {
+                    action = "open_url",
+                    value = "https://schindlershadow.duckdns.org/infiniX/"
+                }
+            }}
+            local json = textutils.serialiseJSON(message)
+            -- sleep(1)
+            chat.sendFormattedMessage(json, "Server Chatbox", "<>", "&b")
+        elseif message and message:lower():find("discord") then
+            local message = {{
+                text = "InfiniX Discord Link",
+                underlined = true,
+                color = "aqua",
+                clickEvent = {
+                    action = "open_url",
+                    value = "https://discord.gg/5vySCrG"
+                }
+            }}
+            local json = textutils.serialiseJSON(message)
+            -- sleep(1)
+            chat.sendFormattedMessage(json, "Server Chatbox", "<>", "&b")
+        end
+
     end
 
-    sleep(5)
 end
+parallel.waitForAny(monitorChat, spawnMessageLoop)
